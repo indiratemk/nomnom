bb63c69b261a82250b251866c321a6e83f0875bb
Seitmagambet Olzhas <sei7magambet@gmail.com>
2018-10-21 18:03:48 +0600

    feature: admin panel

---
+++ nomnom-frontend/src/views/Admin/AdminCategories.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-expansion-panel>
+      <v-expansion-panel-content
+        v-for="category in categories"
+        :key="category.key"
+      >
+        <div slot="header">{{ category.title }}</div>
+        <v-card>
+          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </v-card-text>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+
+
+  export default {
+    name: "AdminCategories",
+    data() {
+      return {
+        categories: null
+      };
+    },
+    created() {
+      axios.get("http://localhost:8085/api/categories")
+        .then(({data}) => this.categories = data)
+        .catch(console.error);
+    }
+  };
+</script>
+<style scoped>
+
+</style>
---
+++ nomnom-frontend/src/views/Admin/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <admin-header/>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import "material-design-icons-iconfont/dist/material-design-icons.css";
+  import "vuetify/dist/vuetify.min.css";
+
+  import Vue from "vue";
+  import Vuetify from "vuetify";
+
+  import AdminHeader from "@views/Admin/AdminHeader";
+
+
+  Vue.use(Vuetify);
+
+  export default {
+    name: "Admin",
+    components: {
+      AdminHeader
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
